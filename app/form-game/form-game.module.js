6ef7d043e3e335e01dc590ac69cfddc68a7c269b
openska <vanessa.kovalsky@free.fr>
2018-11-28 14:01:11 +0100

    Ajout appel post pour soumission formulaire

--- app/form-game/form-game.module.js
+++ app/form-game/form-game.module.js
@@ -4,11 +4,29 @@ angular.module('formGame',[])
         bindings: {
             jeu : '<'
         },
-        controller: function(){
+        controller: function($http){
             this.SubmitJeu = function(){
                 console.log('formulaire soumis');
                 console.log(this.jeu);
-                $http.post('/jeu/:id', $jeu);
+                this.jeu.id=null;
+                $http.post('https://jsonplaceholder.typicode.com/posts', this.jeu).then(
+                    function(response){
+                        console.log(response);
+                    },
+                    function(response){
+                        console.log('erreur');
+                        console.log(response);
+                    }
+                );
+                $http.get('https://jsonplaceholder.typicode.com/posts').then(
+                    function(response2){
+                        console.log(response2);
+                    },
+                    function(response2){
+                        console.log('erreur');
+                        console.log(response2);
+                    }
+                );
 
             }
         }
