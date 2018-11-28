001b8f8fc3bd9129eca6878dc266a3559518d40c
openska <vanessa.kovalsky@free.fr>
2018-11-28 10:23:48 +0100

    Ajout lien vers le detail

--- app/collection/collection.module.js
+++ app/collection/collection.module.js
@@ -3,8 +3,8 @@ angular.module("collectionJeux",['detailGame'])
         templateUrl: 'collection/collection.template.html',
         controller: function(){
             this.array_games = [
-            {titre:'Jeu 1', description:'Jeu 1', image:'img/test.jpg', editeur: "Asmodée"},
-            {titre:'Jeu 2', description:'Jeu 2', image:'img/test.jpg', editeur: "Hasbro"},
+            {id:1, titre:'Jeu 1', description:'Jeu 1', image:'img/test.jpg', editeur: "Asmodée"},
+            {id:2, titre:'Jeu 2', description:'Jeu 2', image:'img/test.jpg', editeur: "Hasbro"},
             ];
         }
     }
