02dd9967a92b33ad6358200ed6ffdc6b6bb3455d
vdavid2 <vanessa.david@alten.com>
2019-04-09 15:15:25 +0200

    Ajout controller nav

--- tp/tp1/corrige/app/app.js
+++ tp/tp1/corrige/app/app.js
@@ -1,1 +1,10 @@
 var app = angular.module("app",[]);
+
+app.controller("NavController",function(){
+    var vm = this;
+    vm.menu = {
+        'liste des jeux' : 'liste-jeux.html',
+        'crééer un jeu' : 'creer-jeu.html',
+        'contact' : 'contact.html'
+    };
+})
